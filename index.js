1c8916760784a253a1544d2ea38086affeda647b
Paul Brown <paulbrown389@gmail.com>
2023-11-14 00:46:00 -0500

    Function lab

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string) {
+    // todo
+  }
+  function shout(string) {
+    return string;
+  }
+  "Hello!".toUpperCase()
+  function shout(string) {
+    return string.toUpperCase();  
+}
+function whisper(string) {
+    return string.toLowerCase()
+}
+function logShout(string) {
+    console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase())
+}
+function sayHiToHeadphonedRoommate(string) {
+   var cantHear = "I can't hear you!"
+   var yesIndeed = "YES INDEED!"
+   var loveTo = "I would love to!"
+    if (string.toLowerCase(string) === string) {
+        return cantHear
+    }
+    else if (string.toUpperCase(string)=== string) {
+        return yesIndeed
+    } 
+    if ("Let's have dinner together!" === string) {
+        return loveTo
+    }
+}
